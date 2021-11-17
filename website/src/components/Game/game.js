7c6ef1e2e8c08b237a69f39ccdfa8f5f252c2ce0
Niriot <lopezpatyyarely@gmail.com>
2021-11-17 15:11:30 -0600

    routes implemented for nav pages.

---
+++ website/src/components/Game/game.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GamePage() {
+  return (
+    <>
+      <h1>GamePage</h1>
+    </>
+  );
+}
+
+export default GamePage;
---
+++ website/src/components/Pokedex/pokedex.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PokedexPage() {
+  return (
+    <>
+      <h1>PokedexPage</h1>
+    </>
+  );
+}
+
+export default PokedexPage;
